4e56f7f29892ac5be9738e6ae2e77c9bd9873e28
DavidIraheta <david.iraheta.89@gmail.com>
2024-09-16 10:59:40 -0600

    Add files via upload

---
+++ 06_01_pseudocode.py
@@ -0,0 +1,14 @@
+# Your cat just had kittens! Now you want to put up an ad to give them
+# to your friends. You'll need to save all names of the kittens,
+# confirm that they each of them is cute, and show a message that
+# that kitten is ready for adoption.
+#
+# Break this task up into a couple of steps of pseudocode
+# and write the pseudocode below in code comments.
+# You don't need to write any functional code, just map out the steps.
+# Kittens available for adoption!
+# 3 available adorable kittens for adoption
+# 1 adorable orange and white who loves to cuddle
+# 1 Spunky and cute all white kitten who loves to play
+# 1 sweet black and white longhair that loves to sleep and snack
+# All kittens are 8 weeks with shots and naccinations ready to be picked up
